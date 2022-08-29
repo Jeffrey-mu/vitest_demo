98dc33b29844a2f4085e1e6744da5d08c7ec1945
Jeffrey <1115378579@qq.com>
2022-08-29 18:44:51 +0800

    test: 测试无法通过 dom渲染是异步

--- src/components/TodoApp/index.vue
+++ src/components/TodoApp/index.vue
@@ -3,15 +3,28 @@
     <div v-for="todo in todos" :key="todo.id" data-test="todo">
       {{ todo.text }}
     </div>
+    <form data-test="form" @submit.prevent="createTodo">
+      <input data-test="new-todo" v-model="newTodo" />
+      <button type="submit"></button>
+    </form>
   </div>
 </template>
 
 <script setup lang="ts" name="TodoApp">
-const todos = [
+import { ref } from "vue";
+const todos = ref([
   {
     id: 1,
     text: "Learn Vue.js 3",
     completed: false,
   },
-];
+]);
+const newTodo = ref("");
+function createTodo() {
+  todos.value.push({
+    id: 2,
+    text: newTodo.value,
+    completed: false,
+  });
+}
 </script>
--- src/components/__tests__/TodoApp.spec.ts
+++ src/components/__tests__/TodoApp.spec.ts
@@ -19,5 +19,28 @@ describe('renders a todo', () => {
     `)
     expect(todo.text()).toBe('Learn Vue.js 3')
   })
+  it('creates a todo', async () => {
+    const wrapper = mount(TodoApp)
+    expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(1)
+
+    wrapper.get('[data-test="new-todo"]').setValue('New todo')
+    wrapper.get('[data-test="form"]').trigger('submit')
+    expect(wrapper.findAll('[data-test="todo"]')).toMatchInlineSnapshot(`
+      [
+        DOMWrapper {
+          "isDisabled": [Function],
+          "wrapperElement": <div
+            data-test="todo"
+          >
+            Learn Vue.js 3
+          </div>,
+        },
+      ]
+    `)
+    // test: 测试无法通过 .toHaveLength为 1
+    // dom更新为异步
+    expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2)
+
+  })
 
 })
